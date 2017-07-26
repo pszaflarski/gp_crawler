6d3fff04a9a5412b61f3301d2d1e83a9e29ebf90
pjacob <pjacob@hubba.com>
2017-07-26 17:52:47 -0400

    crawl_one is complete

--- gp_crawler.py
+++ gp_crawler.py
@@ -5,6 +5,48 @@ without needing to call any of the other modules in this project
 
 """
 
+from selenium_wrapper import *
+
+
+from urllib.parse import urljoin
+
+def get_links(tree, current_url):
+    e = tree.xpath("//a/@href")
+
+    links_on_page = set(rel_to_abs_l(current_url, [x for x in e]))
+
+    e = tree.xpath("//*[contains(@src,'.htm')]/@src")
+
+    links_on_page.update(set(rel_to_abs_l(current_url, [x for x in e])))
+
+    return links_on_page
+
+def rel_to_abs_l(base_url, l):
+    abs_links = []
+    for link in l:
+        if is_relative(link):
+            abs_links.append(urljoin(base_url, link))
+        else:
+            abs_links.append(link)
+
+    return abs_links
+
+def is_relative(url):
+    return ':' not in url
+
+def is_internal(base_url, url):
+    try:
+        internal = {i:x for i,x in enumerate(base_url.split('/'))}.get(2,base_url)
+        test_domain = {i:x for i,x in enumerate(url.split('/'))}.get(2,url)
+        is_internal = (internal in test_domain) or (test_domain in internal)
+    except:
+        is_internal = False
+
+    return is_internal
+
+
+
+
 
 class Crawler:
     def __init__(self):
@@ -32,14 +74,48 @@ class Crawler:
         cache_file = cache_folder + "\\cachemap.csv"
         scrape_strategy = {}
 
-    def crawl_one(self, page):
+        # webdriver
+        self.driver = None
+
+    def crawl_one(self, page, start_url = None):
         # takes: a valid address to a webpage
         # returns: a dictionary with:
         #   'internal': internal links
         #   'external': external links
         #   'page_source': the source for the page
         #   'url': the url that the page ended up at after it finished loading
-        pass
+
+        if start_url is None:
+            # this will guess at what the start url should be
+            start_url = page.split('/')[2]
+
+        print(start_url)
+
+        self.driver = get(self.driver, page)
+
+        source = page_source(self.driver)
+
+        tree = etree_pipeline_fromstring(source)
+
+        current_url = self.driver.current_url
+        links = get_links(tree, current_url)
+
+        external = set([x for x in links if not is_internal(start_url, x)])
+        internal = set([x for x in links if x not in external])
+
+        out_dict = {
+            'internal': internal,
+            'external': external,
+            'page_source': source,
+            'url': current_url
+        }
+
+        return  out_dict
+
+
+
+
+
 
     def crawl_site(self, site):
         # takes: a valid address to a website
@@ -49,4 +125,8 @@ class Crawler:
 
 
 if __name__ == '__main__':
-    pass
+    c = Crawler()
+
+    d = c.crawl_one('http://www.google.ca')
+
+    print(d)
--- selenium_wrapper.py
+++ selenium_wrapper.py
@@ -1,53 +1,204 @@
-"""
-The plan is to make this module and object a container for all the variables in this project.
-Once finished, a user should be able to just use this module to do all their crawling and scraping
-without needing to call any of the other modules in this project
+import os
+import time
+import csv
+import json
+import datetime
+import platform
+import subprocess
 
-"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+import selenium.common.exceptions as selenium_exceptions
+from selenium.webdriver.chrome.options import Options
 
-import selenium_wrapper
+from lxml import html
+from lxml import etree
+from html import escape
+
+import boto3
+
+from openpyxl import Workbook
+
+def get(driver, url, retries = 3):
+
+    if driver is None: driver = init_webdriver()
+
+    for i in range(retries):
+        try:
+            driver.get(url)
+            break
+        except:
+            driver.quit()
+            del driver
+            driver = init_webdriver()
+            continue
+
+    return driver
+
+def page_source(driver):
+    try:
+        a = driver.find_elements_by_xpath('/*')
+        return '\n'.join([x.get_attribute('outerHTML') for x in a])
+    except:
+        return driver.page_source
+
+def to_number(s):
+    try:
+        sl = s.lower().replace(',', '').replace(' ', '')
+        if 'k' in sl:
+            mult = 1000
+        elif 'm' in sl:
+            mult = 1000000
+        else:
+            mult = 1
+
+        r = int(float(sl.replace('m', '').replace('k', '')) * mult)
+        return r
+    except:
+        return None
 
 
-class Crawler:
-    def __init__(self):
-        # common info
-        s3_creds = {}
-        s3_bucket = ""
-        postgres_creds = {}
-        postgres_path = ""
-        postgres_resume_path = ""
+def init_webdriver(headless=True):
+    if platform.system() == 'Windows':
+        chromedriver = "chromedriver.exe"
+    elif platform.system() == 'Linux':
+        subprocess.call('sudo chmod +x ./chromedriver', shell=True)
+        chromedriver = "./chromedriver"
 
-        # crawl variables
-        num_crawler_workers = 4
-        max_crawl_per_site = 10000
+    os.environ["webdriver.chrome.driver"] = chromedriver
 
-        # memory watcher variables
-        max_percent = 75
-        min_free = 500000000
-        wait_step = 3
-        session_name = "work"
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("test-type")
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("--js-flags=--expose-gc")
+    chrome_options.add_argument("--enable-precise-memory-info")
+    chrome_options.add_argument("--disable-popup-blocking")
+    chrome_options.add_argument("--disable-default-apps")
+    chrome_options.add_argument("test-type=browser")
+    chrome_options.add_argument("disable-infobars")
+    if headless:
+        chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(chromedriver, chrome_options=chrome_options)
 
-        # sync/scrape variables
-        num_sync_workders = 10
-        num_scrape_workers = 10
-        cache_folder = 'cache'
-        cache_file = cache_folder + "\\cachemap.csv"
-        scrape_strategy = {}
+    return driver
 
-    def crawl_one(self, page):
-        # takes: a valid address to a webpage
-        # returns: a dictionary with:
-        #   'internal': internal links
-        #   'external': external links
-        #   'page_source': the source for the page
-        #   'url': the url that the page ended up at after it finished loading
-        pass
 
-    def crawl_site(self, site):
-        # takes: a valid address to a website
-        # returns: Nothing
-        # requies: a database and file system to store source files and page data
-        pass
+def get_from_s3(sourcefile_name, bucket, cred_dict):
+    aws_access_key_id = cred_dict['aws_access_key_id']
+    aws_secret_access_key = cred_dict['aws_secret_access_key']
+
+    s3 = boto3.resource(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    ob = s3.Object(bucket, sourcefile_name)
+    return ob.get()["Body"].read().decode('utf-8')
+
+
+def file_to_s3(filename, file_data, bucket, cred_dict):
+    aws_access_key_id = cred_dict['aws_access_key_id']
+    aws_secret_access_key = cred_dict['aws_secret_access_key']
+
+    s3 = boto3.resource(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    ob = s3.Object(bucket, filename)
+    return ob.put(Body=file_data)
+
+
+def delete_s3_file(filename, bucket, cred_dict):
+    aws_access_key_id = cred_dict['aws_access_key_id']
+    aws_secret_access_key = cred_dict['aws_secret_access_key']
+
+    s3 = boto3.resource(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    ob = s3.Object(bucket, filename)
+    return ob.delete()
+
+
+def load_creds(filename):
+    return json.load(open(filename, 'r', encoding='utf-8', errors='ignore'))
+
+
+def etree_pipeline(driver):
+    source = page_source(driver)
+    tree = etree_pipeline_fromstring(source)
+
+    return tree
+
+
+def etree_pipeline_fromstring(source):
+    parser = etree.HTMLParser(encoding='utf-8')
+    u = source.encode()
+    tree = etree.fromstring(u, parser=parser)
+    return tree
+
+
+def login_to_facebook(driver, email, password):
+    driver.get("https://www.facebook.com/")
+    a = driver.find_element_by_id("email").click()
+    ActionChains(driver).send_keys(email).perform()
+    driver.find_element_by_id("pass").click()
+    ActionChains(driver).send_keys(password).perform()
+    ActionChains(driver).send_keys(Keys.ENTER).perform()
+    time.sleep(1)
+
+
+def login_to_instagram(driver):
+    # requires that you are logged into facebook
+
+    driver.get("https://www.instagram.com/accounts/login/")
+
+    a = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.XPATH, "//span[contains(text(),'Log in as')]")))
+    a.click()
+
+    time.sleep(2)
+
+
+def clean_handle(handle):
+    clean = handle.split('/')[-1].replace('@', '').lower()
+    return clean
+
+
+def write_dict_to_csv(filename, fieldnames, d=None, mode='a'):
+    if mode == 'w':
+        f = open(filename, 'w', encoding="utf-8", errors="ignore")
+        writer = csv.DictWriter(f,
+                                lineterminator="\n",
+                                fieldnames=fieldnames)
+        writer.writeheader()
+        f.close()
+    if d is not None:
+        f = open(filename, 'a', encoding="utf-8", errors="ignore")
+        writer = csv.DictWriter(f,
+                                lineterminator="\n",
+                                fieldnames=fieldnames)
+        writer.writerow(d)
+        f.close()
+
+def csv_to_xl(csv_file_source, xlsx_filename, encoding = 'utf-8'):
+    wb = Workbook(write_only=True)
+    ws = wb.create_sheet()
+
+    with open(csv_file_source, 'r', encoding=encoding, errors='ignore') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            ws.append(row)
+
+    wb.save(xlsx_filename)
+
 
 
 if __name__ == '__main__':
