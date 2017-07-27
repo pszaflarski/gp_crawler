17cb4dd0b7f80e6d4200125e1fa1d287524021e2
pjacob <pjacob@hubba.com>
2017-07-27 16:13:04 -0400

    working on crawl_site
    using urllib to find relative links and paths

--- gp_crawler.py
+++ gp_crawler.py
@@ -7,15 +7,15 @@ without needing to call any of the other modules in this project
 
 from selenium_wrapper import *
 
-
 from urllib.parse import urljoin
+from urllib.parse import urlparse
 
 def get_links(tree, current_url):
-    e = tree.xpath("//a/@href")
+    e = tree.xpath("//*/@href")
 
     links_on_page = set(rel_to_abs_l(current_url, [x for x in e]))
 
-    e = tree.xpath("//*[contains(@src,'.htm')]/@src")
+    e = tree.xpath("//*/@src")
 
     links_on_page.update(set(rel_to_abs_l(current_url, [x for x in e])))
 
@@ -36,16 +36,16 @@ def is_relative(url):
 
 def is_internal(base_url, url):
     try:
-        internal = {i:x for i,x in enumerate(base_url.split('/'))}.get(2,base_url)
-        test_domain = {i:x for i,x in enumerate(url.split('/'))}.get(2,url)
-        is_internal = (internal in test_domain) or (test_domain in internal)
+        p = urlparse(url)
+
+        netloc_equal_condition = urlparse(base_url).netloc == p.netloc
+        no_scheme_condition = p.scheme is None or p.scheme == ''
+
+        is_internal = netloc_equal_condition or no_scheme_condition
     except:
         is_internal = False
 
     return is_internal
-
-
-
 
 
 class Crawler:
@@ -78,6 +78,12 @@ class Crawler:
         self.driver = None
 
     def crawl_one(self, page, start_url = None):
+        """ <-- this is so much better, use this!!
+
+        :param page:
+        :param start_url:
+        :return:
+        """
         # takes: a valid address to a webpage
         # returns: a dictionary with:
         #   'internal': internal links
@@ -86,10 +92,9 @@ class Crawler:
         #   'url': the url that the page ended up at after it finished loading
 
         if start_url is None:
-            # this will guess at what the start url should be
-            start_url = page.split('/')[2]
-
-        print(start_url)
+            # this will guess at what the start_url should be
+            p = urlparse(page)
+            start_url = "{scheme}://{netloc}".format(scheme = p.scheme, netloc = p.netloc)
 
         self.driver = get(self.driver, page)
 
@@ -101,7 +106,7 @@ class Crawler:
         links = get_links(tree, current_url)
 
         external = set([x for x in links if not is_internal(start_url, x)])
-        internal = set([x for x in links if x not in external])
+        internal = set([urljoin(start_url,x) for x in links if x not in external])
 
         out_dict = {
             'internal': internal,
@@ -112,21 +117,68 @@ class Crawler:
 
         return  out_dict
 
-
-
-
-
-
-    def crawl_site(self, site):
+    def crawl_site(self, site, resume = True, prioritize = True, max_site_size = 20000):
         # takes: a valid address to a website
         # returns: Nothing
         # requies: a database and file system to store source files and page data
-        pass
+        # OPTIONAL PARAMETERS:
+        # resume: if True, uses the database to find out where the crawler left off, and continues from there
+        # prioritize: if True, will prioritize urls with a path that that does not exist in visited and will
+        #   deprioritize any url that has a path that matches any one that has already been crawled for example
+        #   if 'google.com' has already been crawled, then 'google.com/?q=help' will be deprioritized
+        # max_site_size: limit for number of pages to crawl in a site, default is 20000, if it's -1, the crawler will
+        #   not stop crawling until there are no pages left to crawl
+
+        base_url_format = "{scheme}://{netloc}"
+        base_url_path_format = "{scheme}://{netloc}/{path}"
+
+        progress_dict = {
+            'to_visit': {site},
+            'to_visit_low_priority': set(),
+            'visited': set()
+        }
+
+        url = progress_dict['to_visit'].pop()
+
+        p = urlparse(url)
+        base_url_path = base_url_path_format.format(scheme = p.scheme, netloc = p.netloc, path = p.path)
+
+
+
+        r = self.crawl_one(url)
+
+
+
+        print(r['internal'])
+
+        print(progress_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
+
     c = Crawler()
 
-    d = c.crawl_one('http://www.google.ca')
+    d = c.crawl_site('https://soredgear.com/')
 
-    print(d)
+
+    e = 'mailto://pjacob@hubba.com'
+    u = 'https://soredgear.com/'
+
+    j = urljoin(u,e)
+
+    print(j)
+
+
