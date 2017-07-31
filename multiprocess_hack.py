55e8ff18bd2fc16dba3cadd48a56e6d4d70d5f75
pjacob <pjacob@hubba.com>
2017-07-31 16:01:58 -0400

    simplified the multiprocess to just be a simple wrapper

--- multiprocess_hack.py
+++ multiprocess_hack.py
@@ -8,36 +8,6 @@ from sqlalchemy import *
 
 from gp_crawler import *
 
-
-def create_queue(it, db_file='./queue.db', table_name='queue'):
-    with open(db_file, 'w'):
-        pass
-
-    df = pandas.DataFrame(it)
-    engine_string = 'sqlite:///{db_file}'.format(db_file=db_file)
-    db = create_engine(engine_string)
-    df.to_sql(con=db, index_label='id', name=table_name, if_exists='replace')
-
-
-def get_job_from_queue(index, db_file='queue.db', table_name='queue'):
-    engine_string = 'sqlite:///{db_file}'.format(db_file=db_file)
-    db = create_engine(engine_string)
-    metadata = MetaData()
-
-    t = Table(table_name, metadata, autoload=True, autoload_with=db)
-
-    d = dict(list(db.execute(t.select().where(t.c.id == index)))[0])
-
-    return d
-
-
-def multijob(i):
-    j = get_job_from_queue(i)
-    jid = {x: y for x, y in j.items() if x != 'id'}
-
-    cr = Crawler()
-    cr.crawl_site(**jid)
-
 def wrapper(d):
     cr = Crawler()
     cr.crawl_site(**d)
@@ -47,8 +17,6 @@ def wrapper(d):
 if __name__ == '__main__':
     url_list = [{'start_url': 'http://chef5minutemeals.com/'}, {'start_url': 'http://slapyamama.com/'},
                 {'start_url': 'https://soredgear.com/'}]
-    create_queue(url_list)
-    index_list = list(range(len(url_list)))
 
     pool = multiprocessing.Pool(processes=2)
 
