277f237245ff1a287d01d0d6a425747a8b73e90b
pjacob <pjacob@hubba.com>
2017-08-10 17:10:05 -0400

    multiprocess wrapper

---
+++ db_connector_multiprocess_wrapper.py
@@ -0,0 +1,17 @@
+from db_connector import *
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-params', action='store')
+    args = parser.parse_args()
+
+    if args.params is None:
+        cdc = CrawlerDataConnector()
+        cdc.cache_to_db()
+    else:
+
+        params = json.loads(args.params)
+        cdc = CrawlerDataConnector(**params)
+        cdc.cache_to_db()
