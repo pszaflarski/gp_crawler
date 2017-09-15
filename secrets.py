0f4cebbe530409f6855a92be908c1b6aa40a9e6a
pjacob <pjacob@hubba.com>
2017-09-15 13:46:59 -0400

    added secrets template

---
+++ secrets.py
@@ -0,0 +1,13 @@
+S3_CREDS = {
+    "aws_access_key_id": "",
+    "aws_secret_access_key": ""
+}
+
+SCHEMA = ""
+PAGE_DATA_TABLE_NAME = ""
+PROGRESS_DATA_TABLE_NAME = ""
+S3_BUCKET = ""
+CONNECTION_STRING = ""
+
+FILE_PATH='./cache/'
+
